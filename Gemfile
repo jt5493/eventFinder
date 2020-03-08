dc177de348a31ed5198482c112d395c3b96fc649
jordon_terry <jt5493@hotmail.com>
2020-03-07 23:05:00 -0800

    installed gems

--- Gemfile
+++ Gemfile
@@ -1,8 +1,4 @@
 source "https://rubygems.org"
 gem 'httparty', '~> 0.18.0'
 gem 'pry', '~> 0.12.2'
-
-# Specify your gem's dependencies in eventFinder.gemspec
-gemspec
-
 gem "rake", "~> 12.0"
--- lib/eventFinder/scraper.rb
+++ lib/eventFinder/scraper.rb
@@ -10,7 +10,7 @@ class EventFinder::Scraper
     def fetch_data
         url = https://app.ticketmaster.com/discovery/v2/events.json?postalCode=#{zip}&apikey=O2lM7TkBpe1kU7hgOJPbPDrh3pt49sh9
         response = HTTParty.get(url)
-        response.parsed_response
+        binding.pry
     end
 
 

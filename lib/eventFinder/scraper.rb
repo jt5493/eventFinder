11eb60dd2d8423593d7db2cbb53081df15991278
jordon_terry <jt5493@hotmail.com>
2020-03-08 21:14:36 -0700

    working on Api stuff

--- lib/eventFinder/scraper.rb
+++ lib/eventFinder/scraper.rb
@@ -1,16 +1,9 @@
 class EventFinder::Scraper
-    attr_accessor :zip
-
-    def initialize(zip)
-        @zip = zip
-
-    end
-
 
     def fetch_data
         url = https://app.ticketmaster.com/discovery/v2/events.json?postalCode=#{zip}&apikey=O2lM7TkBpe1kU7hgOJPbPDrh3pt49sh9
         response = HTTParty.get(url)
-        binding.pry
+        binding.pry 
     end
 
 
